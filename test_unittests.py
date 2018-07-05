a6e22f64927bd89b88729e18b9404627664ad0ff
Kory Mathewson <korymath@google.com>
2018-07-05 15:45:14 -0600

    adding basic unit tests

---
+++ test_unittests.py
@@ -0,0 +1,12 @@
+from run import *
+
+def test_imports():
+    assert INCHES_TO_EMU == 914400
+
+def test_get_definitions():
+    definitions = get_definitions('dog')
+    assert len(definitions) == 8
+
+def test_get_synonyms():
+    synonyms = get_synonyms('dog')
+    assert len(synonyms) == 30
