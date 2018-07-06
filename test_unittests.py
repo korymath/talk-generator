7326dc543b68030835c381066f522fb661f26391
T. Winters <info@thomaswinters.be>
2018-07-06 03:01:45 +0200

    Formatted tests

--- test_unittests.py
+++ test_unittests.py
@@ -1,11 +1,14 @@
 from run import *
 
+
 def test_imports():
     assert INCHES_TO_EMU == 914400
+
 
 def test_get_definitions():
     definitions = get_definitions('dog')
     assert len(definitions) == 8
+
 
 def test_get_synonyms():
     synonyms = get_synonyms('dog')
