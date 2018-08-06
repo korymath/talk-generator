781261d048860a25615f3c5b5acbfebcebfd9d83
Thomas Winters <info@thomaswinters.be>
2018-08-06 04:07:39 +0200

    Added TemplateTextGenerator tests

--- test_unittests.py
+++ test_unittests.py
@@ -24,7 +24,33 @@ def test_variable_extraction():
     assert [] == text_generator.get_format_variables("this test only has {} some {} empty names")
 
 
+def test_not_using_unusable_template():
+    """ Tests if the generator is not raising an error when variables are missing to generate, and only uses other
+    generator """
+    possible_templates = ["This is {adjective}", "This is {noun}"]
+    templated_text_generator = text_generator.TemplatedTextGenerator(templates_list=possible_templates)
+    for _ in range(100):
+        assert "This is possible" == templated_text_generator.generate({"adjective": "possible"});
+    for _ in range(100):
+        assert "This is a test" == templated_text_generator.generate({"noun": "a test"});
+
+
+def test_all_possible_outcomes():
+    possible_templates = ["This is {adjective}", "This is {noun}"]
+    templated_text_generator = text_generator.TemplatedTextGenerator(templates_list=possible_templates)
+    expected = {"This is possible", "This is a test"}
+    all_generations = set()
+    for _ in range(10000):
+        if all_generations == expected:
+            break
+        all_generations.add(templated_text_generator.generate({"adjective": "possible", "noun": "a test"}))
+
+    assert expected == all_generations
+
+
 test_some_imports()
 test_get_definitions()
 test_get_synonyms()
 test_variable_extraction()
+test_not_using_unusable_template()
+test_all_possible_outcomes()
