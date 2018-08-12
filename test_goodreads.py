5c33480da3d85b6bcd26b99043a7c9d06474899d
Thomas Winters <info@thomaswinters.be>
2018-08-12 21:54:56 +0200

    Added test for when asking too many good reads quotes

--- test_goodreads.py
+++ test_goodreads.py
@@ -9,6 +9,11 @@ class GoodReadsTest(unittest.TestCase):
         # Check if starts with quote marks
         self.assertEqual('"', cat_quotes[0][0])
 
+    def test_too_many_quotes_amount(self):
+        quotes = goodreads.search_quotes("nine tails", 100)
+        # Check if starts with quote marks
+        self.assertTrue(bool(quotes) and len(quotes) > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
