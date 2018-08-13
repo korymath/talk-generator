53f39face75b96370f9a727039280d73795654fd
Thomas Winters <info@thomaswinters.be>
2018-08-13 13:18:31 +0200

    Added generator_util

--- run.py
+++ run.py
@@ -16,11 +16,13 @@ from google_images_download import google_images_download
 import goodreads
 # Own modules:
 import language_util
-import random_util
 import reddit
 import slide_templates
 import text_generator
 import wikihow
+# Import a lot from generator_util to make schema creation easier
+from generator_util import seeded_generator, none_generator, create_static_generator, combined_generator, \
+    seeded_identity_generator
 from presentation_schema import PresentationSchema, SlideGenerator, constant_weight, create_peaked_weight
 
 MAX_PRESENTATION_SAVE_TRIES = 100
@@ -139,50 +141,6 @@ class SynonymTopicGenerator:
 
     def generate_seed(self, slide_nr):
         return self._seeds[slide_nr]
-
-
-# == TRIVIAL GENERATORS ==
-
-def seeded_generator(simple_generator):
-    return lambda presentation_context: simple_generator(presentation_context["seed"])
-
-
-def none_generator(_):
-    return None
-
-
-def identity_generator(input_word):
-    return input_word
-
-
-def create_static_generator(always_generate_this):
-    return lambda _: always_generate_this
-
-
-def create_none_generator():
-    return lambda _: None
-
-
-def combined_generator(weighted_generators):
-    def generate(seed):
-        current_weighted_generators = list(weighted_generators)
-        while len(current_weighted_generators) > 0:
-            generator = random_util.weighted_random(current_weighted_generators)
-            generated = generator(seed)
-            if generated is not None:
-                return generated
-            _remove_object_from_weighted_list(current_weighted_generators, generator)
-
-    return generate
-
-
-def _remove_object_from_weighted_list(current_weighted_generators, generator):
-    for i in current_weighted_generators:
-        if i and i[1] == generator:
-            current_weighted_generators.remove(i)
-
-
-seeded_identity_generator = seeded_generator(identity_generator)
 
 
 # == CONTENT GENERATORS ==
