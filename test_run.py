798cd8965b241ea5f2dc58982d6a9266dbe8b72d
Thomas Winters <info@thomaswinters.be>
2018-08-16 00:28:23 +0200

    Added test that generates a powerpoint

--- test_run.py
+++ test_run.py
@@ -1,17 +1,29 @@
 import unittest
+from unittest import mock
+import random
 
-from run import *
+import run
 
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_some_imports(self):
-        self.assertTrue(bool(random))
-        self.assertTrue(bool(slide_templates))
-        self.assertTrue(bool(presentation_schema))
+    def setUp(self):
+        random.seed(1)
 
     def test_google_images(self):
-        self.assertTrue(bool(get_related_google_image({"seed": "cat"})))
+        self.assertTrue(bool(run.get_related_google_image({"seed": "cat"})))
+
+    def test_main(self):
+        arguments = mock.Mock()
+        arguments.configure_mock(topic='cat')
+        arguments.configure_mock(num_slides=10)
+        arguments.configure_mock(schema='default')
+        arguments.configure_mock(output_folder="./test_output/")
+        arguments.configure_mock(open_ppt=False)
+        arguments.configure_mock(save_ppt=True)
+        ppt = run.main(arguments)
+
+        self.assertEqual(10, len(ppt.slides))
 
 
 if __name__ == '__main__':
