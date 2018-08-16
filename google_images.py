962f9506964c71c3f222b0c808446e538c4222c0
Thomas Winters <info@thomaswinters.be>
2018-08-16 21:36:40 +0200

    Updated requirements for certain types of google images

--- google_images.py
+++ google_images.py
@@ -1,27 +1,56 @@
 from google_images_download import google_images_download
 
-
 # == CONTENT GENERATORS ==
 # These functions generate content, sometimes related to given arguments
 
-def get_full_screen_google_image(word, num_images=5):
-    paths = []
+
+_FULL_SCREEN_ARGUMENTS = {
+    'exact_size': '1600,900',
+}
+
+_WIDE_ARGUMENTS = {
+    'size': '>1024*768',
+    'aspect_ratio': 'wide',
+}
+
+
+def create_full_screen_image_generator(num_images=5):
+    return lambda word: search_images(word, _FULL_SCREEN_ARGUMENTS, num_images)
+
+
+def create_wide_image_generator(num_images=5):
+    return lambda word: search_images(word, _WIDE_ARGUMENTS, num_images)
+
+
+def create_image_generator(num_images=5):
+    return lambda word: search_images(word, None, num_images)
+
+
+def search_images(word, extra_arguments_dict=None, num_images=5):
     # Get related images at 16x9 aspect ratio
     response = google_images_download.googleimagesdownload()
     arguments = {
         'keywords': word,
         'limit': num_images,
         'print_urls': False,
-        'exact_size': '1600,900',
-        'output_directory': './downloads/google_images/'
+        'print_paths': False,
+        'output_directory': './downloads/google_images/',
+        'language': 'English',
+        'safe_search': True,
+        'no_numbering': True,
     }
+
+    # Add more arguments
+    if extra_arguments_dict is not None:
+        arguments.update(extra_arguments_dict)
+
     # passing the arguments to the function
     paths_dict = response.download(arguments)
+    paths = []
     for value in paths_dict.values():
         paths.extend(value)
 
     # printing absolute paths of the downloaded images
-    # print('paths of images', paths)
     return paths
 
 # @lru_cache(maxsize=20)
