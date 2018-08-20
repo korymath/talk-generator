2b0b37fe4b0762a40c92f2419c35834bc69f4137
Thomas Winters <info@thomaswinters.be>
2018-08-20 21:11:46 +0200

    Added some testcases for to_ing_form

--- language_util.py
+++ language_util.py
@@ -37,19 +37,19 @@ def _make_ing_form(passed_string):
     vowel = ['a', 'e', 'i', 'o', 'u']
     consonant = [c for c in letter if c not in vowel]
     exception = ['be', 'see', 'flee', 'knee', 'lie']
-    if passed_string.endswith('e'):
+
+    if passed_string.endswith('ie'):
+        passed_string = passed_string[:-2]
+        return passed_string + 'ying'
+
+    elif passed_string.endswith('e'):
         if passed_string in exception:
             return passed_string + 'ing'
         else:
             passed_string = passed_string[:-1]
             return passed_string + 'ing'
 
-    elif passed_string.endswith('ie'):
-        passed_string = passed_string[:-2]
-        return passed_string + 'ying'
-
     elif passed_string.endswith('y'):
-        passed_string = passed_string[:-2]
         return passed_string + 'ing'
 
     elif passed_string[-1] in consonant and passed_string[-2] in vowel and passed_string[-3] in consonant:
--- test_language_util.py
+++ test_language_util.py
@@ -20,6 +20,14 @@ class LanguageUtilTest(unittest.TestCase):
         self.assertEqual("cat", language_util.to_singular("cat"))
         self.assertEqual("cat", language_util.to_singular("cats"))
 
+    def test_ing(self):
+        self.assertEqual("toying", language_util.to_ing_form("toy"))
+        self.assertEqual("playing", language_util.to_ing_form("play"))
+        self.assertEqual("lying", language_util.to_ing_form("lie"))
+        self.assertEqual("flying", language_util.to_ing_form("fly"))
+        self.assertEqual("fleeing", language_util.to_ing_form("flee"))
+        self.assertEqual("making", language_util.to_ing_form("make"))
+
 
 if __name__ == '__main__':
     unittest.main()
