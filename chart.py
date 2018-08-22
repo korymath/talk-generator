f4a6634e9373139e1b616ca161bdd29b362227ce
Thomas Winters <info@thomaswinters.be>
2018-08-23 01:38:14 +0200

    Added doughnut chart as alternative

--- chart.py
+++ chart.py
@@ -46,6 +46,18 @@ def create_equal_data_with_outlier_end(size, noise_factor, normal_min, normal_ma
 
 
 # CHART TYPES PROPERTIES SETTING
+
+def _set_pie_label_positions(chart, series, chart_data, label_position):
+    chart.plots[0].has_data_labels = True
+    for i in range(len(chart_data.categories)):
+        point = series.points[i]
+        value = series.values[i]
+        point.data_label.text_frame.text = "{} ({:.0%})".format(chart_data.categories[i].label,
+                                                                value)
+        if label_position:
+            point.data_label.position = label_position
+
+
 def set_histogram_properties(chart, chart_data):
     value_axis = chart.value_axis
     value_axis.mayor_tick_mark = XL_TICK_MARK.NONE
@@ -62,15 +74,8 @@ def set_histogram_properties(chart, chart_data):
 
 def set_pie_properties(chart, chart_data):
     if chart and chart_data:
-        # chart.legend.position = XL_LEGEND_POSITION.RIGHT
-        # chart.legend.include_in_layout = False
-
-        chart.plots[0].has_data_labels = True
-        data_labels = chart.plots[0].data_labels
-        data_labels.number_format = '0%'
-        data_labels.position = XL_LABEL_POSITION.CENTER
-
         chart.has_legend = False
+        chart.has_title = False
 
         # Data points
         series = chart.series[0]
@@ -79,22 +84,24 @@ def set_pie_properties(chart, chart_data):
             t < 0.10 for t in series.values) else XL_LABEL_POSITION.CENTER
 
         # set labels to contain category and value
-        for i in range(len(chart_data.categories)):
-            point = series.points[i]
-            value = series.values[i]
-            point.data_label.text_frame.text = "{} ({:.0%})".format(chart_data.categories[i].label,
-                                                                    value)
+        _set_pie_label_positions(chart, series, chart_data, label_position)
 
-            point.data_label.position = label_position
+
+def set_doughnut_properties(chart, chart_data):
+    if chart and chart_data:
+        chart.has_legend = False
+        series = chart.series[0]
+        _set_pie_label_positions(chart, series, chart_data, None)
 
 
 # CHART TYPES
 PIE = XL_CHART_TYPE.PIE, set_pie_properties
 PROCENT_HISTOGRAM = XL_CHART_TYPE.COLUMN_CLUSTERED, set_histogram_properties
+DOUGHNUT = XL_CHART_TYPE.DOUGHNUT, set_doughnut_properties
 
 # CHART DATA GENERATOR
 
-_YES_NO_CHART_TYPES = PIE, PROCENT_HISTOGRAM
+_YES_NO_CHART_TYPES = PIE, PROCENT_HISTOGRAM, DOUGHNUT
 
 
 def generate_yes_no_large_funny_answer_chart_data(presentation_context):
@@ -107,7 +114,7 @@ def generate_yes_no_large_funny_answer_chart_data(presentation_context):
 
     chart_data = ChartData()
     chart_data.categories = categories
-    chart_data.add_series('Answers', series_data)
+    chart_data.add_series("", series_data)
     return title, chart_data
 
 
@@ -118,5 +125,5 @@ def generate_yes_no_single_answer_chart_data(presentation_context):
 
 def generate_yes_no_pie(presentation_context):
     title, chart_data = generate_yes_no_large_funny_answer_chart_data(presentation_context)
-    type, chart_modifier = random.choice(_YES_NO_CHART_TYPES)
-    return title, type, chart_data, chart_modifier
+    chart_type, chart_modifier = random.choice(_YES_NO_CHART_TYPES)
+    return title, chart_type, chart_data, chart_modifier
