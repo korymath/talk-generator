37435bf9c60b38bd4f788a0f33de865915bc9569
Kory Mathewson <korymath@google.com>
2018-11-14 10:05:58 -0700

    ensure pip upgraded

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 virtualenv -p python3 venv
 source venv/bin/activate
+pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 -m nltk.downloader wordnet punkt averaged_perceptron_tagger
