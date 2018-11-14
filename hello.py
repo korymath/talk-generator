23fb51899fda2ec5ca3e4aa157507255510f4633
Kory Mathewson <korymath@google.com>
2018-11-14 11:49:05 -0700

    adding a template for a basic form

--- hello.py
+++ hello.py
@@ -1,10 +1,36 @@
 from flask import Flask
 from flask_sslify import SSLify
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
+
+# App config.
+DEBUG = True
 app = Flask(__name__)
 sslify = SSLify(app)
-
-
-@app.route('/')
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+ 
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+ 
+ 
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return 'Hello, World!'
+    form = ReusableForm(request.form)
+ 
+    print(form.errors)
+    if request.method == 'POST':
+        name=request.form['name']
+        print(name)
+ 
+        if form.validate():
+            # Save the comment here.
+            flash('Hello ' + name)
+        else:
+            flash('All the form fields are required. ')
+ 
+    return render_template('hello.html', form=form)
+ 
+if __name__ == "__main__":
+    app.run()
