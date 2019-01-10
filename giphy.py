5e14553d768c3749f55a706a9939e98f5be2059b
Thomas Winters <thomas.winters@kuleuven.be>
2019-01-10 18:03:56 +0100

    Extracted giphy

---
+++ giphy.py
@@ -0,0 +1,24 @@
+import safygiphy
+import os
+import os_util
+
+giphy = safygiphy.Giphy()
+
+
+def get_related_giphy(seed_word):
+    if bool(seed_word):
+        response = giphy.random(tag=seed_word)
+    else:
+        response = giphy.random()
+
+    if bool(response):
+        data = response.get('data')
+        if bool(data):
+            images = data.get('images')
+            original = images.get('original')
+            giphy_url = original.get('url')
+            gif_name = os.path.basename(os.path.dirname(giphy_url))
+            image_url = 'downloads/giphy/' + str(seed_word) + "/" + gif_name + ".gif"
+            os_util.download_image(giphy_url, image_url)
+            return image_url
+
--- schemas.py
+++ schemas.py
@@ -1,8 +1,8 @@
 import os
 import random
-import safygiphy
 
 # Own modules:
+import giphy
 import chart
 import goodreads
 import google_images
@@ -143,30 +143,9 @@ weird_and_shitpost_generator = combined_generator(
 
 # GIFS
 
-giphy = safygiphy.Giphy()
-
-
-def get_related_giphy(seed_word):
-    if bool(seed_word):
-        response = giphy.random(tag=seed_word)
-    else:
-        response = giphy.random()
-
-    if bool(response):
-        data = response.get('data')
-        if bool(data):
-            images = data.get('images')
-            original = images.get('original')
-            giphy_url = original.get('url')
-            gif_name = os.path.basename(os.path.dirname(giphy_url))
-            image_url = 'downloads/giphy/' + str(seed_word) + "/" + gif_name + ".gif"
-            os_util.download_image(giphy_url, image_url)
-            return image_url
-
-
 giphy_generator = create_backup_generator(
-    create_seeded_generator(get_related_giphy),
-    lambda _: get_related_giphy(None)
+    create_seeded_generator(giphy.get_related_giphy),
+    lambda _: giphy.get_related_giphy(None)
 )
 reddit_gif_generator = create_reddit_image_generator("gifs", "gif", "gifextra", "nonononoYES")
 
