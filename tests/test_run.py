3f80206eec32f44a8b1acf2753305eca2a336673
Thomas Winters <thomas.winters@kuleuven.be>
2019-01-14 17:14:47 +0100

    Fixed error with output folder

--- tests/test_run.py
+++ tests/test_run.py
@@ -2,7 +2,7 @@ import unittest
 from unittest import mock
 import random
 
-from talkgenerator import run
+from talkgenerator import run, os_util
 from talkgenerator import schemas
 
 
@@ -19,7 +19,7 @@ class TestTalkGenerator(unittest.TestCase):
         arguments.configure_mock(topic='cat')
         arguments.configure_mock(num_slides=10)
         arguments.configure_mock(schema='default')
-        arguments.configure_mock(output_folder="../test_output/")
+        arguments.configure_mock(output_folder=os_util.to_actual_file("../test_output/"))
         arguments.configure_mock(open_ppt=False)
         arguments.configure_mock(save_ppt=True)
         ppt = run.main(arguments)
