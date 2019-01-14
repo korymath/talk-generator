b7318e3b2af8fe454048b7522f849a2776c94938
Thomas Winters <thomas.winters@kuleuven.be>
2019-01-14 15:54:00 +0100

    Fixes to time_test

--- talkgenerator/time_test.py
+++ talkgenerator/time_test.py
@@ -3,24 +3,27 @@ import time
 from talkgenerator import os_util
 from talkgenerator import run
 
-words = os_util.read_lines("../data/eval/common_words.txt")[0:3]
-result_file = open("../data/eval/timings.txt", "a+")
 
-for topic in words:
-    arguments = run.get_argument_parser().parse_args(
-        ['--topic', topic, '--num_slides', '7', '--save_ppt', 'False', '--open_ppt', 'False'])
+def run_time_test(number_of_times):
+    words = os_util.read_lines("../data/eval/common_words.txt")[0:number_of_times]
+    result_file = open("../data/eval/timings.txt", "a+")
 
-    start = time.process_time()
-    clock_start = time.perf_counter()
+    for topic in words:
+        arguments = run.get_argument_parser().parse_args(
+            ['--topic', topic, '--num_slides', '7', '--save_ppt', 'False', '--open_ppt', 'False'])
 
-    run.main(arguments)
+        start = time.process_time()
+        clock_start = time.perf_counter()
 
-    end = time.process_time()
-    clock_end = time.perf_counter()
-    timing = end - start
-    clock_timing = clock_end - clock_start
-    print("It took {} seconds to generate the presentation, and {} seconds system-wide ".format(str(timing), str(clock_timing)))
-    result_file.write(topic + ", " + str(timing) + ", " + str(clock_timing))
-    result_file.flush()
+        run.main(arguments)
 
-result_file.close()
+        end = time.process_time()
+        clock_end = time.perf_counter()
+        timing = end - start
+        clock_timing = clock_end - clock_start
+        print("It took {} seconds to generate the presentation, and {} seconds system-wide ".format(str(timing),
+                                                                                                    str(clock_timing)))
+        result_file.write(topic + ", " + str(timing) + ", " + str(clock_timing) + '\n')
+        result_file.flush()
+
+    result_file.close()
