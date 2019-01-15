817ffd6b327e680085617f734574818be82f1387
Thomas Winters <thomas.winters@kuleuven.be>
2019-01-15 19:25:29 +0100

    Implemented basic title slide representation

--- talkgenerator/slide/slides.py
+++ talkgenerator/slide/slides.py
@@ -1,13 +1,13 @@
 from abc import ABCMeta, abstractmethod
+from talkgenerator.slide import powerpoint_slide_creator
 
 
 class Slide(metaclass=ABCMeta):
     """ Class representing a slide object that could be used to export to Powerpoint pptx or other representations later
     """
 
-    def __init__(self):
-        # ...
-        pass
+    # def __init__(self):
+    #     pass
 
     @property
     @abstractmethod
@@ -20,3 +20,13 @@ class Slide(metaclass=ABCMeta):
         pass
 
 
+class TitleSlide(Slide):
+    def __init__(self, title, subtitle):
+        self._title = title
+        self._subtitle = subtitle
+
+    def slide_type(self):
+        return powerpoint_slide_creator.LAYOUT_TITLE_SLIDE
+
+    def create_powerpoint_slide(self, prs):
+        return powerpoint_slide_creator.create_title_slide(prs, self._title, self._subtitle)
