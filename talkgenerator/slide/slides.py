fb8f7c787b96580caa2013fccf8f12c38ad55f4e
Thomas Winters <thomas.winters@kuleuven.be>
2019-01-16 15:22:33 +0100

    Made cleaner class structure with less repetition for slide

--- talkgenerator/slide/slides.py
+++ talkgenerator/slide/slides.py
@@ -6,8 +6,8 @@ class Slide(metaclass=ABCMeta):
     """ Class representing a slide object that could be used to export to Powerpoint pptx or other representations later
     """
 
-    # def __init__(self):
-    #     pass
+    def __init__(self, arguments):
+        self._arguments = arguments
 
     @property
     @abstractmethod
@@ -15,28 +15,20 @@ class Slide(metaclass=ABCMeta):
         """ The function converting it to powerpoint"""
         pass
 
-    @property
-    @abstractmethod
-    def arguments(self):
-        pass
-
     def create_powerpoint_slide(self, prs):
         """ Should generate a slide in the powerpoint """
-        return self.ppt_slide_creator(prs, **self.arguments)
+        return self.ppt_slide_creator(prs, **self._arguments)
 
 
 class TitleSlide(Slide):
     def __init__(self, title, subtitle):
-        self._title = title
-        self._subtitle = subtitle
+        super().__init__({
+            'title': title,
+            'subtitle': subtitle
+        })
 
     @property
     def ppt_slide_creator(self):
         return powerpoint_slide_creator.create_title_slide
 
-    @property
-    def arguments(self):
-        return {
-            'title': self._title,
-            'subtitle': self._subtitle
-        }
+#         }
