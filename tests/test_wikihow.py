192ddc24df4123bf087aea190abaa268739dca7f
Thomas Winters <thomas.winters@kuleuven.be>
2019-07-23 15:29:27 +0200

    Fixed bug where wikihow titles sometimes has number of views in title

--- tests/test_wikihow.py
+++ tests/test_wikihow.py
@@ -8,6 +8,15 @@ class MyTestCase(unittest.TestCase):
         actions = wikihow.get_related_wikihow_actions_basic_search("cat")
         self.assertFalse("articles from wikiHow" in actions)
 
+    def test_no_views_in_wikihow_action(self):
+        actions = wikihow.get_related_wikihow_actions("grass")
+        print(actions)
+        for action in actions:
+            # No line breaks allowed
+            self.assertFalse("\n" in action)
+            # No number of views
+            self.assertFalse(" views" in action and "Updated" in action)
+
 
 if __name__ == '__main__':
     unittest.main()
