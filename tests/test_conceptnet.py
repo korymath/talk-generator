7b46d14d0c264bcc03492655656169da571e54cd
Thomas Winters <thomas.winters@kuleuven.be>
2019-07-30 17:22:51 +0200

    Added conceptnet test

---
+++ tests/test_conceptnet.py
@@ -0,0 +1,20 @@
+import unittest
+
+from talkgenerator.schema.slide_topic_generators import SideTrackingTopicGenerator
+from talkgenerator.sources import conceptnet
+
+
+class ConceptNetTest(unittest.TestCase):
+    def test_conceptnet_standard(self):
+        related_words = conceptnet.get_weighted_related_words('cat', 10)
+        print(related_words)
+        self.assertTrue(len(related_words) is 10)
+
+    def test_conceptnet_multiword(self):
+        related_words = conceptnet.get_weighted_related_words('my lap', 10)
+        print(related_words)
+        self.assertTrue(len(related_words) > 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_slide_topic_generators.py
+++ tests/test_slide_topic_generators.py
@@ -4,12 +4,17 @@ from talkgenerator.schema.slide_topic_generators import SideTrackingTopicGenerat
 
 
 class SlideTopicGeneratorsTest(unittest.TestCase):
-    def test_conceptnet_standard(self):
+    def test_conceptnet_sidetracking_standard(self):
         generator = SideTrackingTopicGenerator('cat', 10)
         self.assertTrue(len([seed for seed in generator.all_seeds() if seed is None]) is 0)
 
-    def test_conceptnet_non_existing_topic(self):
+    def test_conceptnet_sidetracking_non_existing_topic(self):
         generator = SideTrackingTopicGenerator('nonexistingword-bla-bla', 10)
+        self.assertTrue(len([seed for seed in generator.all_seeds() if seed is None]) is 0)
+
+    def test_conceptnet_sidetracking_hard_topic(self):
+        generator = SideTrackingTopicGenerator('scratch_furniture', 10)
+        print(generator.all_seeds())
         self.assertTrue(len([seed for seed in generator.all_seeds() if seed is None]) is 0)
 
 
