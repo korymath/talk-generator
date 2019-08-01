27a5e0b0d650d76fdeab1ddd5750446b67071841
Thomas Winters <thomas.winters@kuleuven.be>
2019-08-01 13:18:00 +0200

    Added searching on shitpostbot and getting the ratings

---
+++ tests/test_shitpostbot.py
@@ -0,0 +1,19 @@
+import unittest
+from talkgenerator.sources import unsplash, shitpostbot
+
+
+class ShitPostBot(unittest.TestCase):
+
+    def test_shitpostbot_search(self):
+        image_urls = shitpostbot.search_images('cat')
+        self.assertTrue(len(image_urls) > 0)
+
+    def test_shitpostbot_search_rated(self):
+        image_urls = shitpostbot.search_images_rated('cat')
+        self.assertTrue(len(image_urls) > 0)
+        # Check if the rating of the first one is large
+        self.assertTrue(int(image_urls[0][1]) > 20)
+
+
+if __name__ == '__main__':
+    unittest.main()
