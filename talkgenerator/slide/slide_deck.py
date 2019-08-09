a945bb4df56c45006d41ff31f805a2f141bee38c
Thomas Winters <info@thomaswinters.be>
2019-08-09 14:03:16 +0300

    Added logging to slide_deck

--- talkgenerator/slide/slide_deck.py
+++ talkgenerator/slide/slide_deck.py
@@ -1,4 +1,9 @@
-class SlideDeck():
+import logging
+
+logger = logging.getLogger("talkgenerator")
+
+
+class SlideDeck:
     """ Represents a deck of Slide objects    """
 
     def __init__(self, size):
@@ -14,14 +19,14 @@ class SlideDeck():
     def save_to_powerpoint(self, prs_template):
         """ Should generate a slide in the powerpoint """
         if not self.is_complete():
-            print("ERROR: SOME SLIDES WERE NOT GENERATED:", self._slides)
+            logger.error("ERROR: SOME SLIDES WERE NOT GENERATED: {}".format(self._slides))
             self._slides = [slide for slide in self._slides if slide is not None]
         return [x.create_powerpoint_slide(prs_template) for x in self._slides]
 
     def get_structured_data(self):
         """ Return slide deck as structured data for alternative presentation """
         if not self.is_complete():
-            print("ERROR: SOME SLIDES WERE NOT GENERATED:", self._slides)
+            logger.error("ERROR: SOME SLIDES WERE NOT GENERATED: {}".format(self._slides))
             self._slides = [slide for slide in self._slides if slide is not None]
         return [x for x in self._slides]
 
