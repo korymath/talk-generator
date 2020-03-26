a09d36fc168ce958c517cfadb75c4527f30f5b2a
Thomas Winters <info@thomaswinters.be>
2020-03-26 19:30:10 +0100

    Added extra argument to unpack from generator

--- talkgenerator/run.py
+++ talkgenerator/run.py
@@ -3,7 +3,7 @@ from talkgenerator import generator
 
 def main(args):
     """Main run method for command line talk generation."""
-    presentations, slide_deck = generator.generate_presentation_using_cli_arguments(
+    presentations, slide_deck, output_file = generator.generate_presentation_using_cli_arguments(
         args
     )
 
