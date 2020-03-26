b14d71b77d4be377b312b77419abb37329cf4a14
Thomas Winters <info@thomaswinters.be>
2020-03-26 20:13:31 +0100

    Fixed Inspirobot images

--- talkgenerator/sources/inspirobot.py
+++ talkgenerator/sources/inspirobot.py
@@ -4,7 +4,7 @@ from talkgenerator.util import os_util
 from talkgenerator.datastructures.image_data import ImageData
 
 
-def get_random_inspirobot_image(_):
+def get_random_inspirobot_image(_=None):
     # Generate a random url to access inspirobot
     dd = str(random.randint(1, 73)).zfill(2)
     nnnn = random.randint(0, 9998)
@@ -13,9 +13,9 @@ def get_random_inspirobot_image(_):
     )
 
     # Download the image
-    image_url = os_util.to_actual_file(
-        "downloads/inspirobot/{}-{}.jpg".format(dd, nnnn)
-    )
-    os_util.download_image(inspirobot_url, image_url)
+    # image_url = os_util.to_actual_file(
+    #     "downloads/inspirobot/{}-{}.jpg".format(dd, nnnn)
+    # )
+    # os_util.download_image(inspirobot_url, image_url)
 
-    return ImageData(image_url=image_url, source="Inspirobot")
+    return ImageData(image_url=inspirobot_url, source="Inspirobot")
