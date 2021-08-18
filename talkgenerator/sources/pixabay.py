9da8a641d374be4e7c4072e3eceef1c2a72bbd07
Kory Mathewson <korymath@gmail.com>
2021-08-18 17:49:31 -0400

    Fix for the pixabay adapter.

--- talkgenerator/sources/pixabay.py
+++ talkgenerator/sources/pixabay.py
@@ -1,8 +1,7 @@
 import logging
-from pathlib import Path
+import requests
 from typing import List
 
-from pixabay import Image
 from talkgenerator import settings
 from talkgenerator.datastructures.image_data import ImageData
 
@@ -12,23 +11,32 @@ logger = logging.getLogger("talkgenerator")
 
 def get_pixabay_session():
     creds = settings.pixabay_auth()
-    api = Image(api_key=creds["pixabay_key"])
-    return api
+    api_key = creds["pixabay_key"]
+    return api_key
 
 
 def search_horizontal(query):
     return search_photos(query, orientation="horizontal")
 
 
+def search_vertical(query):
+    return search_photos(query, orientation="vertical")
+
+
 def search_photos(query, orientation="all") -> List[ImageData]:
-    pixabay_session = get_pixabay_session()
-    logger.debug('pixabay_session: {}'.format(pixabay_session))
-    logger.debug('pixabay.search_photos called with query: {}'.format(query))
-    if pixabay_session and query:
-        results = pixabay_session.search(q=query, orientation=orientation)
-        if results and results["hits"]:
+    api_key = get_pixabay_session()
+    logger.debug("pixabay_api_key: {}".format(api_key))
+    query = query.replace(' ', '+')
+    logger.debug("pixabay.search_photos called with query: {}".format(query))
+    url_query = f"https://pixabay.com/api/?key={api_key}&q={query}&image_type=photo&orientation={orientation}"
+    logger.debug("pixabay url_query: {}".format(url_query))
+    if api_key and url_query:
+        results = requests.get(url=url_query)
+        logger.debug("request response results: {}".format(results))
+        response_data = results.json()
+        if results.status_code == 200 and response_data["hits"]:
             images = []
-            for photo in results["hits"]:
+            for photo in response_data["hits"]:
                 link_download = photo["largeImageURL"]
                 creator = photo["user"] + " (via Pixabay)" if "user" in photo else None
                 images.append(ImageData(image_url=link_download, source=creator))
--- tests/test_pixabay.py
+++ tests/test_pixabay.py
@@ -4,7 +4,7 @@ from talkgenerator.sources import unsplash, pixabay
 
 class PixabayTest(unittest.TestCase):
     def test_pixabay_access(self):
-        images = pixabay.search_photos("office")
+        images = pixabay.search_photos("office chair")
         self.assertTrue(len(images) > 0)
         sources = [
             image.get_source() for image in images if image.get_source() is not None
