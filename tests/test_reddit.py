055b4dfe4b338788dfb6f580f542cdee3b734c17
Thomas Winters <info@thomaswinters.be>
2021-11-23 00:45:04 +0100

    Added new reddit test

--- tests/test_reddit.py
+++ tests/test_reddit.py
@@ -1,9 +1,18 @@
 import unittest
 
+from talkgenerator.sources import reddit
 from talkgenerator.schema.content_generator_structures import RedditImageSearcher
 
 
 class RedditTest(unittest.TestCase):
+    def test_reddit_search_image(self):
+        result = reddit.search_subreddit(
+            "memes", str("cat") + " nsfw:no (url:.jpg OR url:.png OR url:.gif)"
+        )
+        print("Result from reddit is", result)
+        self.assertIsNone(result)
+        self.assertTrue(len(result) > 0)
+
     def test_reddit_simple(self):
         images = RedditImageSearcher("memes")("cat")
         self.assertTrue(len(images) > 0)
